2a047edf79fab94735ce74fd186d7983a937d851
awaisirshad1 <awaisirshad1@icloud.com>
2025-02-23 16:47:24 -0500

    generated empty type

--- lab2/src/client_rovers.py
+++ lab2/src/client_rovers.py
@@ -1,12 +1,17 @@
 import grpc
 import ground_control_pb2
 import ground_control_pb2_grpc
+import google
+
 
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = ground_control_pb2_grpc.ground_controlStub(channel)
-        print('Enter the rover number:')
-        rover_number = input()
+        # print('Enter the rover number:')
+        # rover_number = input()
+        empty_param = google.protobuf.empty_pb2.Empty()
+        response = stub.get_map(empty_param)
+        print(response)
 
 
 
--- lab2/src/ground_control_service.py
+++ lab2/src/ground_control_service.py
@@ -6,23 +6,40 @@ import ground_control_pb2_grpc
 import rover_utils as rvr
 
 
-class ground_control_servicer(ground_control_pb2_grpc.ground_controlServicer):
+class GroundControlServicer(ground_control_pb2_grpc.ground_controlServicer):
     # get the map into an array
     def get_map(self, request, context):
-        return rvr.extract_map_into_array('../map_and_mines/map1.txt')
+        print(request)
+        response = ground_control_pb2.land__array__pb2
+        map_array = rvr.extract_map_into_array('../map_and_mines/map1.txt')
+        print(f'map_array:{map_array}')
+        response.values = map_array
+        return response
 
     def get_movements(self, request, context):
+        print(request)
         rover_num = request.rover_number
-        print()
+        print(f'rover_num:{rover_num}')
+
+    def get_mine_serial_number(self, request, context):
+        print(request)
+
+    def rover_status(self, request, context):
+        print(request)
+
+    def share_mine_pin(self, request, context):
+        print(request)
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
-    ground_control_pb2_grpc.add_ground_controlServicer_to_server(ground_control_servicer(), server)
-    server.add_secure_port("localhost:50051")
+    ground_control_pb2_grpc.add_ground_controlServicer_to_server(GroundControlServicer(), server)
+    server.add_insecure_port("localhost:50051")
     server.start()
     server.wait_for_termination()
 
+
 if __name__ == '__main__':
     serve()
+
 
